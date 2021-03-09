19a282f59e57424795e8368b3c1ec5860998f7f6
John Wehrle <wehrlej@gmail.com>
2021-03-08 21:18:29 -0800

    Null safe version of zefyr flutter package. All dependencies from other packages by pulyaevskiy have been included. Not fully tested but ready to add as a package under lib. Should only require changing import paths to work as zefyr package main branch (as of 8 March 2021)

---
+++ zefyr/notus/notus.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2018, the Zefyr project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Rich text document model for Zefyr editor.
+library zefyr.notus;
+
+export 'package:birch/zefyr/notus/src/document.dart';
+export 'package:birch/zefyr/notus/src/document/attributes.dart';
+export 'package:birch/zefyr/notus/src/document/block.dart';
+export 'package:birch/zefyr/notus/src/document/leaf.dart';
+export 'package:birch/zefyr/notus/src/document/line.dart';
+export 'package:birch/zefyr/notus/src/document/node.dart';
+export 'package:birch/zefyr/notus/src/heuristics.dart';
+export 'package:birch/zefyr/notus/src/heuristics/delete_rules.dart';
+export 'package:birch/zefyr/notus/src/heuristics/format_rules.dart';
+export 'package:birch/zefyr/notus/src/heuristics/insert_rules.dart';
